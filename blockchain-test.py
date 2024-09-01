9e55c974060c6c57437acbbdae4652f6860691a0
Pasha Mango <141678664+pashamango@users.noreply.github.com>
2024-08-31 17:07:11 -0700

    Create blockchain-test.py
    Простая инициализация только bitcoinlib

---
+++ blockchain-test.py
@@ -0,0 +1,10 @@
+# blockchain-test.py
+
+import bitcoinlib
+
+def main():
+    # Инициализация и проверка bitcoinlib
+    bitcoinlib.initialize_bitcoinlib()
+
+if __name__ == "__main__":
+    main()
